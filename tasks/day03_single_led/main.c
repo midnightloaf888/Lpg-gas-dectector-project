7ff02b8f84503f5822dcee30bcc619d9740b9c65
midnightloaf888 <aaryanimarakwar@gmail.com>
2026-07-16 16:48:48 +0530

    Day 3 - Implemented single LED blinking using ESP32 GPIO

---
+++ tasks/day03_single_led/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "driver/gpio.h"
+
+#define LED_PIN GPIO_NUM_4
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_PIN);
+
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_PIN, 1);
+        printf("LED ON\n");
+        sleep(1);
+
+        gpio_set_level(LED_PIN, 0);
+        printf("LED OFF\n");
+        sleep(1);
+    }
+}
